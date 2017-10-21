b123c7cf50f079dd94e1432439001329ddc69034
Mahdi Firdaus <pipipapipuu@gmail.com>
2017-10-21 16:55:31 +0700

    Init Repo

---
+++ src/main/java/com/mahdifr/psp/PspTutorial6Application.java
@@ -0,0 +1,12 @@
+package com.mahdifr.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspTutorial6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspTutorial6Application.class, args);
+	}
+}
